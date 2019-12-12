22001adfa34927c79293316b75dcb566120f3811
Feng Shu <tumashu@163.com>
2019-12-12 13:15:13 +0800

    Add company-posframe-pkg.el

---
+++ company-posframe-pkg.el
@@ -0,0 +1,6 @@
+(define-package "company-posframe" "0.1.0"
+  "Use a posframe as company candidate menu."
+  '((emacs "26.0")
+    (company "0.9.0")
+    (posframe "0.1.0")
+    (company-quickhelp "2.2.0")))
