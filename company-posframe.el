b1fc969de5d69fb0c750ef4682533175bb902e42
Feng Shu <tumashu@163.com>
2020-01-02 19:43:40 +0800

    Revert "Add company-posframe-enable-overriding-keymap to deal with company-posframe-active-map"
    This reverts commit 323db9f5810e15895edb18f38c470ee0466adc1c.
    
    This approach do not work, I can not find why ...

--- company-posframe.el
+++ company-posframe.el
@@ -173,13 +173,6 @@ be triggered manually using `company-posframe-quickhelp-show'."
     keymap)
   "Keymap that is enabled during an active completion in posframe.")
 
-(defun company-posframe-enable-overriding-keymap (keymap)
-  "Advice function of `company-enable-overriding-keymap'."
-  (company-uninstall-map)
-  (if (eq keymap company-active-map)
-      (setq company-my-keymap company-posframe-active-map)
-    (setq company-my-keymap keymap)))
-
 (defun company-posframe-format-backend-name (backend)
   "Format BACKEND for displaying in the modeline."
   (propertize (cl-typecase backend
@@ -252,7 +245,12 @@ COMMAND: See `company-frontends'."
          (company-posframe-quickhelp-cancel-timer))
        (company-posframe-quickhelp-hide)
        (company-posframe-hide))
-      (update (company-posframe-show))
+      (update
+       ;; Important: This line is very important, we need to override
+       ;; company-active-map again, this is do the job of
+       ;; `company--perform'.
+       (company-enable-overriding-keymap company-posframe-active-map)
+       (company-posframe-show))
       (post-command
        (when (not run-quickhelp-command-p)
          (company-posframe-show))))))
@@ -409,8 +407,6 @@ just grab the first candidate and press forward."
       (message "company-posframe can not work in current emacs environment.")
     (if company-posframe-mode
         (progn
-          (advice-add #'company-enable-overriding-keymap
-                      :override #'company-posframe-enable-overriding-keymap)
           (advice-add #'company-pseudo-tooltip-frontend
                       :override #'company-posframe-frontend)
           (advice-add #'company-pseudo-tooltip-unless-just-one-frontend
@@ -420,8 +416,6 @@ just grab the first candidate and press forward."
           (message company-posframe-notification))
       (posframe-delete company-posframe-buffer)
       (posframe-delete company-posframe-quickhelp-buffer)
-      (advice-remove #'company-enable-overriding-keymap
-                     #'company-posframe-enable-overriding-keymap)
       (advice-remove #'company-pseudo-tooltip-frontend
                      #'company-posframe-frontend)
       (advice-remove #'company-pseudo-tooltip-unless-just-one-frontend
